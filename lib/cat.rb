830d12c283352ba0a97f6e191762958b32785d1d
Dean L Betty <deanlloyd64@gmail.com>
2019-09-25 14:20:29 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,11 @@
 class Cat
   # code goes here
-  attr_accessor :owner
+  attr_accessor :owner, :mood
   attr_reader :name
   
-  def initialize(name, owner)
+  def initialize(name, owner, mood)
     @name = name
     self.owner = owner
+    @mood = mood
   end
 end
