77569a0ca2384ba33842b8e32cccac145d65bf30
Dean L Betty <deanlloyd64@gmail.com>
2019-09-25 14:26:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,18 @@
 class Dog
   # code goes here
-  attr_accessor :owner
+  attr_accessor :owner, :mood
   attr_writer :name
   
+  
+  @@all = []
+  
   def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = playful
+  end
+  
+  def self.all
+    @@all
+  end
 end
