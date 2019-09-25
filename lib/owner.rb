c9c75302abe9525adda4e8a539e76cf8b19e31c4
Dean L Betty <deanlloyd64@gmail.com>
2019-09-25 14:11:15 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,12 +2,19 @@ class Owner
   # code goes here
   attr_reader :name, :species
   
+  @@all = {}
+  
   def initialize(name)
     @name = name
     @species = "human"
+    @@all << self
   end
   
   def say_species
     "I am a #{species}."
+  end
+  
+  def all
+    @@all
   end
 end
